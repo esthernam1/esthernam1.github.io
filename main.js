d1e1f40d3baa577ac0c2dc46567e1a2e8aa84c27
Esther Nam <name961@newschool.edu>
2019-11-03 16:59:40 -0500

    first comrst commit

--- main.js
+++ main.js
@@ -11,13 +11,16 @@ for (let i = 0; i < segNum; i++) {
 }
 
 function setup() {
-  createCanvas(2000, 2000);
+  createCanvas(1400, 2000);
   strokeWeight(10);
   stroke(255, 100);
 }
 
+
 function draw() {
-  background(242, 243, 235);
+  // background(249, 248, 245);
+  background(255,255,255);
+  // background(234, 234, 0);
   dragSegment(0, mouseX, mouseY);
   for (let i = 0; i < x.length - 1; i++) {
     dragSegment(i + 1, x[i], y[i]);
@@ -42,27 +45,4 @@ function segment(x, y, a) {
   pop();
 }
 
-//if you are on a new page, the line of the color will change
-// function newColor(){
-//   if()
-// }
 
-
-//menu function
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("list");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.menu')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('list')) {
-        openDropdown.classList.remove('list');
-      }
-    }
-  }
-}
